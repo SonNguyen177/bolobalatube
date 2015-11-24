103fef570c874893a8c276a451a55857951070af
bolobalavn <nguyenhoaisonfat@gmail.com>
2015-11-24 11:04:39 +0700

    init

---
+++ app/src/main/java/com/bolobala/bolobalatube/Config.java
@@ -0,0 +1,14 @@
+package com.bolobala.bolobalatube;
+
+/**
+ * Created by nguye on 11/23/2015.
+ */
+public final  class Config {
+    private Config(){
+
+    }
+
+    //
+
+    public static final String YOUTUBE_API_KEY ="AIzaSyBen269vzTWTzYAmAUW_PZuQgsIY1VRL9U";
+}
